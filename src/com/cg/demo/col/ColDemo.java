3ba0dae25fdf2b629c69cbee2945b4daca0e4112
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-10 09:44:52 +0530

    Create ColDemo.java

---
+++ src/com/cg/demo/col/ColDemo.java
@@ -0,0 +1,22 @@
+package com.cg.demo.col;
+
+import java.util.ArrayList;
+
+public class ColDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		ArrayList myList = new ArrayList();
+		System.out.println(myList.size());
+		myList.add(100);
+		myList.add(50.50);
+		myList.add("XYZ");
+		myList.add("ABC");
+		System.out.println(myList.size());
+		System.out.println(myList);
+	}
+		
+	}
+
+
