cd06c6b1188d09caea0918a88ae8590793e751ae
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-10 10:11:17 +0530

    cmt

---
+++ src/com/cg/demo/col/EmpDemo.java
@@ -0,0 +1,25 @@
+package com.cg.demo.col;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpDemo {
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		Delete delete = new Delete();
+		
+		List<Employee> empList = new ArrayList<Employee>();
+		
+		empList.add(emp);
+		empList.add(new Employee());
+		empList.add(new Employee(101, "ABC", 10.5));
+//		empList.add("ABC");
+//		empList.add(delete);
+		
+		System.out.println(empList);
+		
+		
+	}
+
+}
