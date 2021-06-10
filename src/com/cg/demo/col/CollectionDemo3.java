885b85d08bfe29488313506132e181f3126751e3
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-10 12:07:09 +0530

    Create CollectionDemo3.java

---
+++ src/com/cg/demo/col/CollectionDemo3.java
@@ -0,0 +1,19 @@
+package com.cg.demo.col;
+
+import java.util.ArrayList;
+
+public class CollectionDemo3 {
+	public static void main(String[] args) {
+		
+		ArrayList<Comparable> al = new ArrayList<Comparable>();
+		al.add(50);
+		al.add(30.05);
+		al.add("Vivek");
+		al.add(new Integer(20));
+		System.out.println(al);
+		
+			
+		
+	}
+
+}
