5c7325fbd1e80e9b988536f0be673fdc3a09d0bb
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-10 11:55:05 +0530

    Update CollectionDemo2.java

--- src/com/cg/demo/col/CollectionDemo2.java
+++ src/com/cg/demo/col/CollectionDemo2.java
@@ -7,7 +7,7 @@ public class CollectionDemo2 {
 	public static void main(String[] args) {
 		
 		ArrayList<Integer> al = new ArrayList<>();
-		al.add(new Integer(10));
+		al.add(new Integer (10));
 		al.add(20);
 //		al.add(10.5);
 //		al.add(new Float (34.50));
