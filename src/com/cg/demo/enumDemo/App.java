74498256c0050afbe93e0a765c8bb86359f83f9e
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-15 09:23:52 +0530

    Commit
    Commit

---
+++ src/com/cg/demo/enumDemo/App.java
@@ -0,0 +1,12 @@
+package com.cg.demo.enumDemo;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Start");
+		
+		Day today = Day.TUESDAY;
+		System.out.println(today);
+		
+	}
+
+}
---
+++ src/com/cg/demo/enumDemo/Day.java
@@ -0,0 +1,7 @@
+package com.cg.demo.enumDemo;
+
+public enum Day {
+	
+	SUNDAY, MONDAY, TUESDAY, WEDNESDASY, THURSDAY, FRIDAY, SATURDAY
+
+}
