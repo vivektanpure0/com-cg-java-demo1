b6481cb48c90942583fa93799ad6bf78c1a59f80
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-17 15:14:59 +0530

    Create CallableDemo.java

---
+++ src/com/cg/demo/conc/CallableDemo.java
@@ -0,0 +1,35 @@
+package com.cg.demo.conc;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableDemo implements Callable<Integer> {
+//public class CallableDemo implements Callable<Employee> {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		CallableDemo obj = new CallableDemo();
+
+		ExecutorService service = Executors.newSingleThreadExecutor();
+
+		Future<Integer> future = service.submit(obj); // A Future represents the result of an asynchronous computation.
+
+		Integer num = future.get();
+
+		System.out.println(num);
+		service.shutdown();
+	}
+
+	@Override
+	public Integer call() throws Exception {
+//	public Employee call() throws Exception {
+		int num = 0;
+		for (int i = 0; i <= 10; i++) {
+			num += i;
+		}
+		return num;
+	}
+}
