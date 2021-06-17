285b851984091da5f5159f2d3e8d0ec0c721a6ab
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-17 15:14:11 +0530

    Commit
    Commit

---
+++ src/com/cg/demo/conc/CallableDemo2.java
@@ -0,0 +1,37 @@
+package com.cg.demo.conc;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.cg.demo.col.Employee;
+
+public class CallableDemo2 implements Callable<Employee> {
+
+
+
+public static void main(String[] args) throws InterruptedException, ExecutionException {
+	CallableDemo2 obj= new CallableDemo2();
+	ExecutorService service = Executors.newSingleThreadExecutor();
+
+	Future<Employee> future = service.submit(obj); 
+	Employee emp = future.get(); 
+	System.out.println(emp);
+
+	service.shutdown();
+
+	
+}
+
+ @Override
+
+public Employee call() throws Exception {
+	 Employee em = new Employee();
+	 return em;
+	 
+ }
+
+
+}
