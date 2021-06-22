796935ea98f6c111ce28cc9dc772e91dbbffbd4e
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-22 11:34:49 +0530

    Commit
    Commit

--- src/com/cg/java/demo1/App.java
+++ src/com/cg/java/demo1/App.java
@@ -1,8 +1,8 @@
 package com.cg.java.demo1;
 
-public class AddTwoNumbers {
+public class App {
 	public static void main(String[] args) {
-
+		System.out.println("Start");
 		int num1 = 5;
 
 		int num2 = 50;
@@ -10,6 +10,7 @@ public class AddTwoNumbers {
 		int num3 = num1 + num2; 
 
 		System.out.println(num3);
+		System.out.println("End");
 
 		
 	}
---
+++ src/com/cg/java/logs/LogDemo.java
@@ -0,0 +1,14 @@
+package com.cg.java.logs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogDemo {
+		
+	public static void main(String[] args) {
+	    Logger logger = LoggerFactory.getLogger(LogDemo.class);
+	    logger.info("Hello World");
+	  }
+	}
+
+
